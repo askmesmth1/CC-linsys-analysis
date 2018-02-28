de4f62e2c0ead3bcb05c3fa0f2bee7bebdcb9858
space-pilot-3000 <alexusss23@gmail.com>
2018-02-28 21:22:43 +0700

    Срочно нужен фильтр

--- source.py
+++ source.py
@@ -1,6 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import csv
-
+'''
 with open("output.csv", 'w', newline='') as file:
     writer = csv.writer(file)
 
@@ -11,22 +12,90 @@ with open("input.csv", newline='') as file:
     N = np.array(data[2])
     Y = np.array(data[3])
     T = np.array(data[4])
+print(X, '\n', U, '\n', N, '\n', Y, '\n', T, '\n')
+'''
 
-A = np.random.sample(size=(X.size, X.size))
-B = np.random.sample(size=(X.size, U.size))
-E = np.random.sample(size=(X.size, N.size))
-C = np.random.sample(size=(Y.size, X.size))
-D = np.random.sample(size=(Y.size, U.size))
-F = np.random.sample(size=(Y.size, N.size))
+def system_is_stable(a):
+    result = set()
+    for x in range(len(a)):
+        plt.plot([0, a[x].real], [0, a[x].imag], 'bo--')
+        if abs(a[x].real) < 1 and abs(a[x].imag) < 1:
+            result.add('stable')
+        else:
+            result.add('unstable')
+    if 'unstable' in result:
+        return False
+    else:
+        return True
 
-for i in range(T[0]):
+'''Input'''
+with open("input.txt", newline='', encoding='utf-8') as file:
+    data = [rec.replace(' ', '')[3:-1] for rec in file.readlines()]
+    X = np.fromstring(data[0], dtype=float, sep=',')
+    U = np.fromstring(data[1], dtype=float, sep=',')
+    N = np.fromstring(data[2], dtype=float, sep=',')
+    Y = np.fromstring(data[3], dtype=float, sep=',')
+    T = int(data[4])
+
+'''Matrices'''
+A = np.random.normal(size=(X.size, X.size))
+A = A/3
+#A = np.array([[-0.1, 0.3, 0.05, 0.6, 0.8, 0.06], [1, 0.1, -1, 1, 0.2, 1], [-1, -1, -0.6, 1, 0.3, 0.5], [0.07, -0.09, 1, 0.2, -0.5, -0.8], [1, -1, 0.7, 0.9, -0.9, -0.3], [1, 1, 0.8, 0.5, 0.4, 0.2]])
+#A = A/3
+B = np.random.normal(size=(X.size, U.size))
+E = np.random.normal(size=(X.size, N.size))
+C = np.random.normal(size=(Y.size, X.size))
+D = np.random.normal(size=(Y.size, U.size))
+F = np.random.normal(size=(Y.size, N.size))
+
+'''Eigenvalues of square matrix A'''
+p = np.linalg.eigvals(A)
+
+X_files, Y_files, Y_average, time, max_val = [], [], [], [], 0.0
+
+'''Main cycle'''
+for i in range(T):
     N = np.random.normal(size=(N.shape))
-    X = np.matmul(A, X) + np.matmul(B, U) + np.matmul(E, N)
+    X = np.matmul(A, X) + np.matmul(B, U)# + np.matmul(E, N)
     N = np.random.normal(size=(N.shape))
-    Y = np.matmul(C, X) + np.matmul(D, U) + np.matmul(F, N)
-    print('state:', X)
-    print('output:', Y)
+    Y = np.matmul(C, X) + np.matmul(D, U)# + np.matmul(F, N)
 
+    if Y[1] > max_val:
+        max_val = Y[1]
+    elif max_val == 0.0:
+        max_val = Y[1]
+
+    time.append(i)
+    X_files.append(X)
+    Y_files.append(Y)
+    Y_average.append(Y[1])
+    #print('state:', X)
+    #print('output:', Y)
+
+'''Overshoot and setting time'''
+overshoot = abs((max_val - Y_average[-1]) / Y_average[-1])
+for element in reversed(Y_average):
+    if abs((element - Y_average[-1]) / Y_average[-1]) > 0.05:
+        setting_time = Y_average.index(element)
+        break
+
+'''Results'''
+if system_is_stable(p):
+    print('Система устойчива')
+    print('overshoot:', overshoot * 100, '%', '\nmaximum value:', max_val, '\nfinal value:', Y_average[-1], '\nsetting time:', setting_time)
+else:
+    print('Система неустойчива')
+plt.plot(time, Y_average)
+plt.grid()
+plt.show()
+
+'''Output'''
+with open('output.txt', 'w', encoding='utf-8') as file:
+    for line in Y_files:
+        file.write('Y = '+ str(line) + '\n')
+
+'''
     with open("output.csv", 'a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(Y)
+'''
